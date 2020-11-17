dd1f3f83a630469ea664a65cd204a15cd5a2837d
Jonathan Perry <jonathanperry@sequencemedia.net>
2020-11-17 21:56:35 +0000

    `react-tab-set`

--- config/gulp/transform/index.js
+++ config/gulp/transform/index.js
@@ -21,7 +21,7 @@ const STYLESHEETS = /(assets\/stylesheets\/.*-)\d+\.\d+\.\d+(.css)/g
 const getFileData = async (filePath) => readFile(filePath, 'utf8')
 const setFileData = async (filePath, fileData) => writeFile(filePath, fileData, 'utf8')
 
-const getFilePathList = () => new Promise((resolve, reject) => glob(['./public/*.html', './public/**/*/*.html'], (e, a) => !e ? resolve(a) : reject(e)))
+const getFilePathList = () => new Promise((resolve, reject) => glob(['./server/views/*.html', './server/views/**/*/*.html'], (e, a) => !e ? resolve(a) : reject(e)))
 const setFilePathList = (filePathList) => Promise.all(filePathList.map(async (filePath) => setFileData(filePath, (await getFileData(filePath)).replace(JAVASCRIPTS, `$1${version}$2`).replace(STYLESHEETS, `$1${version}$2`))))
 
 export async function transform () {
